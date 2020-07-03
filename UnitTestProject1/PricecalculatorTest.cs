fac750f8f7b5796e95bb663d7439dfff2c30e613
Ravi Bhushan Kumar <Ravibhushan_Kumar@Dellteam.com>
2020-07-03 13:17:27 +0530

    Test Scenerio modified

--- UnitTestProject1/PricecalculatorTest.cs
+++ UnitTestProject1/PricecalculatorTest.cs
@@ -7,30 +7,43 @@ namespace UnitTestProject1
     [TestClass]
     public class PricecalculatorTest
     {
-         public readonly Dictionary<string, Dictionary<int, int>> TestInput = null;  
+         public readonly Dictionary<string, Dictionary<int, int>> TestInput1, TestInput2 , TestInput3 = null;
+       
         public PricecalculatorTest() {
-            TestInput=  new Dictionary<string, Dictionary<int, int>>(){
-                                                                                    {"A",new Dictionary<int, int>(){{50,4}}
-},
-                                                                                    {"B",new Dictionary<int, int>(){{30,3}}},
+            TestInput1 = new Dictionary<string, Dictionary<int, int>>();
+                                                                                    
+            TestInput1.Add("A", new Dictionary<int, int>() { { 50, 1 } });
+            TestInput1.Add("B", new Dictionary<int, int>() { { 30, 1 } });
+            TestInput1.Add("C", new Dictionary<int, int>() { { 20, 1 } });
+            TestInput2 = new Dictionary<string, Dictionary<int, int>>(){
+                                                                                    {"A",new Dictionary<int, int>(){{50,5}}},
+                                                                                    {"B",new Dictionary<int, int>(){{30,5}}},
+                                                                                    {"C",new Dictionary<int, int>(){{20,1}}}
+                                                                                     };
+
+            TestInput3 = new Dictionary<string, Dictionary<int, int>>(){
+                                                                                    {"A",new Dictionary<int, int>(){{50,3}}},
+                                                                                    {"B",new Dictionary<int, int>(){{30,5}}},
                                                                                     {"C",new Dictionary<int, int>(){{20,1}}},
-                                                                                    {"D",new Dictionary<int, int>(){{15,5}}}
-                                                                         };
-        }                                                                     
-        [TestMethod]
+                                                                                    {"D",new Dictionary<int, int>(){{15,1}}} };
+
+        }
+       [TestMethod]
         public void TotalPricewithoutpromo()
         {
             Pricecalculator obj = new Pricecalculator();
-            int totaolprice= obj.TotalPrice(TestInput);
-            Assert.Equals(300, totaolprice);
+            int totaolprice= obj.TotalPrice(TestInput1);
+            Assert.Equals(100, totaolprice);
 
         }
         [TestMethod]
         public void TotalPricewithpromo()
         {
             Pricecalculator obj = new Pricecalculator();
-            int totaolprice = obj.TotalPrice(TestInput,true, "InstantPromo");
-            Assert.Equals(300, totaolprice);
+            int totaolprice = obj.TotalPrice(TestInput2,true, "InstantPromo");
+            Assert.Equals(370, totaolprice);
+            int totaol = obj.TotalPrice(TestInput3, true, "InstantPromo");
+            Assert.Equals(280, totaol);
         }
     }
 }
