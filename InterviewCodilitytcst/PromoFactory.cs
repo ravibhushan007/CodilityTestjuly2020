3751cbc2b61de6f040d417ec239b9c4a533533ae
Ravi Bhushan Kumar <Ravibhushan_Kumar@Dellteam.com>
2020-07-03 14:36:23 +0530

    taking leverage of Factory patter to create intance according active promotion

---
+++ InterviewCodilitytcst/PromoFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewCodilitytcst
+{
+    abstract class PromoFactory
+    {
+        public abstract IPromotional PromoIntance(string Promotype);
+    }
+}
---
+++ InterviewCodilitytcst/PromotionFactoryObj.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewCodilitytcst
+{
+    class PromotionFactoryObj : PromoFactory
+    {
+        public override IPromotional PromoIntance(string Promotype)
+        {
+
+            switch (Promotype)
+            {
+                case "InstantPromo":
+                    return new InstantPromo();
+                case "CashBackPromo":
+                    return new CashBackPromo();
+                default:
+                    throw new ApplicationException(string.Format("Promotype '{0}' cannot be created", Promotype));
+            }
+
+        }
+    }
+}
